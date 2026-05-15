26ff7626def703f040c45eda7e6585b07586fbbc
reetarora1906-debug <ritikaaroraa2004@gmail.com>
2026-05-15 23:04:42 +0530

    optimize chunking

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -16,4 +16,13 @@ export default defineConfig({
   optimizeDeps: {
     include: ['react-is'],
   },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom', 'recharts', 'axios'],
+        },
+      },
+    },
+  },
 });
